cc402a356f0071d9d174a1bdc1bb41b21dd85b53
Inframatic <isteffy@hotmail.com>
2013-07-17 15:17:06 -0400

    Making tests for browsing projects pass

---
+++ test/integration/project_flows_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class ProjectFlowsTest < ActionDispatch::IntegrationTest
+  test "browsing projects" do
+  	laser_gun = FactoryGirl.create(:project, :title => "Laser Gun")
+  	stun_ray = FactoryGirl.create(:project, :title => "Stun Ray")
+  	death_ray = FactoryGirl.create(:project, :title => "Death Ray")
+
+  	#go to page with products
+  	visit "/projects"
+  	#Assert that the page we're on is the page for the projects
+  	assert_equal projects_path, current_path
+
+  	#Assert this page has the words 'Listing Projects'
+  	assert page.has_content?('Listing projects')
+
+  	#Assert this page has these words as well
+  	assert page.has_content?('Laser Gun')
+  	assert page.has_content?('Stun Ray')
+  	assert page.has_content?('Death Ray')
+  # test "the truth" do
+  #   assert true
+  # end
+  end
+end
