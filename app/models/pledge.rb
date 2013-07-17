026f3b10f8ba4ad5c96cee6f43b3fcc4e5c6831e
Inframatic <isteffy@hotmail.com>
2013-07-17 18:50:37 -0400

    Generating factory for Pledge

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,4 +1,11 @@
 class Pledge < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
+	  FactoryGirl.define do
+	  factory :pledge do
+	    amount 100
+	    user
+	    project
+	  end
+	end
 end
